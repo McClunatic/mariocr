2b078233028ae2da49e4865ed24e929c58639794
Brian McClune <bpmcclune@gmail.com>
2021-06-18 13:32:49 -0400

    Add logFiles method to test input @change

--- src/components/FileUploadButton.vue
+++ src/components/FileUploadButton.vue
@@ -1,5 +1,26 @@
 <template>
   <label>
-    Select Files<input id="files" type="file" multiple hidden>
+    Select Files
+    <input
+      id="files"
+      type="file"
+      multiple
+      hidden
+      @change=logFiles
+    >
   </label>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const logFiles = event => {
+      console.log(event.target.files)
+    }
+
+    return { logFiles }
+  },
+})
+</script>
